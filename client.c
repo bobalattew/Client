aa2b8034aaf8ec8ef604dcfd191542c3b1c2ddbc
marykrshaa <48324374+marykrshaa@users.noreply.github.com>
2019-03-19 15:51:03 +0800

    Update client.c

--- client.c
+++ client.c
@@ -14,7 +14,7 @@ int main(){
      struct sockaddr_in server_address;
      server_address.sin_family=AF_INET;
      server_address.sin_port=htons(9002);
-     sever_address.sin_addr.s_addr=inet_addr("10.0.2.15");
+     server_address.sin_addr.s_addr=inet_addr("10.0.2.15");
      
      int stat=connect(network_socket,(struct sockaddr*) &server_address,sizeof(server_address));
      
