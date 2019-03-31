35d492af6020b2fc679c54b1d605e59ec02a0c8f
marykrshaa <48324374+marykrshaa@users.noreply.github.com>
2019-03-31 14:59:30 +0800

    Update and rename clientn.c to client.c

--- client.c
+++ client.c
@@ -13,22 +13,19 @@ void error(const char *msg)
     exit(0);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
     char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
+    
+    portno=9002;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    server = 10.0.2.15;
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
